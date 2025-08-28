58e3914af61ebc1b2debfdebcc8647d74250089e
Lucas <lucassilvadpaula@gmail.com>
2025-08-28 01:52:56 -0300

    primeira página

---
+++ choosedText.js
@@ -0,0 +1,15 @@
+ const texto = `E lá vamos nós… vou usar toda a capacidade da minha memória para escrever e te dedicar esta carta do fundo do meu coração. Você é muito mais do que posso expressar em palavras. Você é uma das poucas pessoas com quem me aproximei de verdade, alguém em quem pude me sentir seguro para amar e confiar.
+
+Desde o primeiro momento que te vi, eu te apelidei de "a menina da calça azul" para o Carlos. Seu jeito tímido de desviar o olhar era o que mais me encantava. Até que, em um certo dia, sentei ao seu lado e perguntei: “Que horas são?” Nunca imaginei que uma pergunta tão simples despertaria em mim o desejo de conhecer cada detalhe sobre você.
+
+No dia seguinte, você não apareceu, e o Carlos comentou que você tinha pegado o mesmo ônibus que ele. Naquele instante, senti uma tristeza enorme, imaginando que talvez eu tivesse te assustado. Mas então você apareceu novamente, radiante, e eu achei melhor não incomodar… embora por dentro eu estivesse morrendo de vontade de conversar.
+
+Mais tarde, quando você veio falar comigo no Sinduscon, meus olhos brilhavam e meu sorriso reluzia. Cheguei ao Carlos e disse: “Adivinha quem veio falar comigo hoje?” Ele perguntou: “Quem?” E eu respondi: "A menina da calça azul".
+
+Lembro do nosso primeiro encontro no Food Square. Eu estava nervoso, mas amei aquela noite. Foi o palco do nosso primeiro beijo, e você estava linda. Conheci mais de você, seus gostos, sua família, e você conheceu a minha. Rimos, brincamos e cada detalhe ficou guardado como em um filme de amor, vivendo nosso próprio “Um Amor de Cinema”.
+
+Quando você viajou para Ushuaia, senti saudade, mas pensei que cada momento distante só aumentava meu carinho por você. Lembrei do colar que você gostou e fui atrás dele como uma surpresa, guardando como um tesouro. Cada momento com você é precioso, cada pequeno detalhe importa, e me faz querer conhecer mais de você, da sua essência.
+
+Você é a verdadeira Adina, sem filtros, e me sinto sortudo por ter te conhecido. Você é o melhor presente que já recebi, e cada dia ao seu lado parece preencher minha própria lista de vida.
+
+Gosto do seu jeito doce, do seu jeito tímido de perguntar algo, do seu sorriso que ilumina qualquer ambiente. Me encanto com quem você é. Tento fazer tudo o que não fizeram por você, porque sei que você merece. Te amo muito, Adina, e dedico esta carta a você.`;
